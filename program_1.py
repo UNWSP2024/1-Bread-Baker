4c7a2194fbc8bf94633bbae7c3551a7b40c7deeb
Bread-Baker <aefafach@students.unwsp.edu>
2026-01-13 16:12:11 -0600

    Updated 'Modified Hello Part 1'

--- program_1.py
+++ program_1.py
@@ -1,3 +1,7 @@
+# Name: Ariana Fafach
+# Date: 1/13/2026
+# Title: Modified Hello Part 1
+
 def hello_world():
     # Update the message below to say "Hello World", 
     # Python cares about white space, so watch your indentation.
